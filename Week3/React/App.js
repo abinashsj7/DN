f76474cd710966bbc2ae6c8b663673d208dca43d
abinashsj7 <sit23cs075@sairamtap.edu.in>
2026-07-18 20:22:25 +0530

    Update App.js

--- Week3/React/App.js
+++ Week3/React/App.js
@@ -1,1 +1,30 @@
 
+function App() {
+  return (
+    <div>
+      <h1>Student Management System</h1>
+
+      <h3>Add Student</h3>
+
+      <input type="text" placeholder="Enter Name" />
+      <br /><br />
+
+      <input type="text" placeholder="Enter Department" />
+      <br /><br />
+
+      <button>Add Student</button>
+
+      <hr />
+
+      <h3>Students</h3>
+
+      <ul>
+        <li>Abinash - CSE</li>
+        <li>Rahul - IT</li>
+        <li>Priya - ECE</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
