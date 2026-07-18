8e7073fbb3dd19245f04ccab8fc70aea58bdfbf6
abinashsj7 <sit23cs075@sairamtap.edu.in>
2026-07-18 18:42:42 +0530

    Update app.py

--- Week2/Flask/app.py
+++ Week2/Flask/app.py
@@ -3,18 +3,29 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 students = [
-    {"id": 1, "name": "Alice"},
-    {"id": 2, "name": "Bob"},
-    {"id": 3, "name": "Charlie"}
+    {"id": 1, "name": "Alice", "department": "CSE"},
+    {"id": 2, "name": "Bob", "department": "ECE"},
+    {"id": 3, "name": "Charlie", "department": "IT"}
 ]
+
 
 @app.route("/")
 def home():
     return "Welcome to Flask Student API"
 
+
 @app.route("/students")
 def get_students():
     return jsonify(students)
+
+
+@app.route("/students/<int:id>")
+def get_student(id):
+    for student in students:
+        if student["id"] == id:
+            return jsonify(student)
+    return jsonify({"message": "Student not found"}), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
