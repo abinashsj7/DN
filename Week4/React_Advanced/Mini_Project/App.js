495ce2065a274514a21f8db264a8b03768f38b1c
abinashsj7 <sit23cs075@sairamtap.edu.in>
2026-07-18 20:57:11 +0530

    Update App.js

--- Week4/React_Advanced/Mini_Project/App.js
+++ Week4/React_Advanced/Mini_Project/App.js
@@ -1,1 +1,30 @@
+import { useState, useEffect } from "react";
+import Student from "./Student";
 
+function App() {
+  const [students] = useState([
+    { id: 1, name: "Abinash", department: "CSE" },
+    { id: 2, name: "Rahul", department: "IT" },
+    { id: 3, name: "Priya", department: "ECE" }
+  ]);
+
+  useEffect(() => {
+    console.log("Student Management App Loaded");
+  }, []);
+
+  return (
+    <div>
+      <h1>Advanced Student Management</h1>
+
+      {students.map((student) => (
+        <Student
+          key={student.id}
+          name={student.name}
+          department={student.department}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
