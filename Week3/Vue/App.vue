d268cf16355c108bc7522717f7383474a1915721
abinashsj7 <sit23cs075@sairamtap.edu.in>
2026-07-18 20:30:36 +0530

    Create App.vue for Student Management application

---
+++ Week3/Vue/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Student Management</h1>
+    <p>Welcome to Vue.js!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style>
+h1 {
+  color: blue;
+}
+</style>
