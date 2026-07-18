4ab620bcb8315dcacea84884f609e9d8118423d6
abinashsj7 <sit23cs075@sairamtap.edu.in>
2026-07-18 21:11:08 +0530

    Update Google_Search.py

--- Week5/Selenium/Google_Search.py
+++ Week5/Selenium/Google_Search.py
@@ -1,1 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
+driver = webdriver.Chrome()
+
+driver.get("https://www.google.com")
+
+search = driver.find_element(By.NAME, "q")
+search.send_keys("Cognizant Digital Nurture")
+search.send_keys(Keys.RETURN)
+
+print(driver.title)
+
+driver.quit()
